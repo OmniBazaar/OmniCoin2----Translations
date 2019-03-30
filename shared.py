36836d4fd36a784e2d3eb52a8d41af18f6e8624a
Tu Tran <tu.t@scopicsoftware.com>
2019-03-30 16:21:13 +0700

    Add new languages and update messages

--- shared.py
+++ shared.py
@@ -1,15 +1,17 @@
 import os
 import json
+from shutil import copyfile
 
 translations_root = os.getcwd()
-locales_root = '/Users/denissamohvalov/Documents/scopic-development/omnibazaar-ui/app/dist/i18n/app'
-app_root = '/Users/denissamohvalov/Documents/scopic-development/omnibazaar-ui/app'
-locales = ['en', 'es', 'fr', 'it', 'ru']
-
+#locales_root = '/Users/denissamohvalov/Documents/scopic-development/omnibazaar-ui/app/dist/i18n/app'
+#app_root = '/Users/denissamohvalov/Documents/scopic-development/omnibazaar-ui/app'
+locales_root = '/Users/ptu/develop/scopic/oc2/omnibazaar-ui/app/dist/i18n/app'
+app_root = '/Users/ptu/develop/scopic/oc2/omnibazaar-ui/app'
+locales = ['en', 'zh', 'hi', 'es', 'fr', 'ar', 'ru', 'pt', 'id', 'tr', 'ko', 'vi', 'it', 'gu', 'tl', 'ms', 'ro', 'uk']
 
 def save(obj, path):
     with open(path, 'w') as out:
-        out.write(json.dumps(obj, indent=4))
+        out.write(json.dumps(obj, indent=4, ensure_ascii=False))
 
 
 def read_obj(path):
@@ -23,3 +25,10 @@ def get_translations(locales):
         with open(os.path.join(translations_root, '%s.json' % locale)) as json_data:
             translations[locale] = json.loads(json_data.read())
     return translations
+
+def add_new_langs():
+    for locale in locales:
+        path = os.path.join(translations_root, '%s.json' % locale)
+        exists = os.path.isfile(path)
+        if not exists:
+            copyfile(os.path.join(translations_root, 'en.json'), path)
